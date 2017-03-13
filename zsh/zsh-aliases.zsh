0eb317695fe399b101bfd911bdfb0043460208be
Teddy Ku <teddypku@gmail.com>
2017-03-13 14:39:59 -0400

    alias youtube-dl

--- zsh/zsh-aliases.zsh
+++ zsh/zsh-aliases.zsh
@@ -12,7 +12,12 @@ alias -g G='| grep' # now you can do: ls foo G something
 # Functions
 #
 # (f)ind by (n)ame
-# usage: fn foo 
+# usage: fn foo
 # to find all files containing 'foo' in the name
 function fn() { ls **/*$1* }
+
+alias yt='noglob yt'
+function yt() {
+  youtube-dl $1 &>/dev/null &
+}
 
