a76163507e1fe4fec7e0d7e4a5011ae61c6d0043
Teddy Ku <teddypku@gmail.com>
2017-03-15 12:54:17 -0400

    fix rvm path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,10 @@
 # path, the 0 in the filename causes this to load first
 path=(
+  $GEM_HOME/bin
+  /usr/local/opt/openssl/bin
   $path
   $HOME/.yadr/bin
   $HOME/.yadr/bin/yadr
+  $HOME/.rvm/bin
 )
 
